d29b37d2819d0acc4f4b8bbf5913ada242069191
mahade <owork.hasan@gmail.com>
2026-04-03 21:26:04 +0600

    commited

---
+++ src/pages/Home/BreakingNews.jsx
@@ -0,0 +1,11 @@
+const BreakingNews = () => {
+  return (
+    <div>
+      <div className="border-2 border-gray-600 breakingNews">
+        <button className="btn btn-secondary">Latest</button>
+      </div>
+    </div>
+  );
+};
+
+export default BreakingNews;
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,17 @@
+import logo from "../../../assets/logo.png";
+import moment from "moment-timezone";
+
 const Header = () => {
+  const currentDate = moment().format("dddd, MMMM D YYYY");
+
   return (
-    <div className="border-2 border-gray-600">
-      <h2>Header</h2>
+    <div className="border-2 border-gray-600 ">
+      <div className="flex items-center justify-center p-4">
+        {" "}
+        <img src={logo} alt=""></img>
+      </div>
+      <p className="text-center">Journalism Without Fear and Favour</p>
+      <p className="text-center">{currentDate}</p>
     </div>
   );
 };
