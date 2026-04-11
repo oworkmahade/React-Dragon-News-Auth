af18330527d23bf2f3f8a1d9718fa2bdb9bd64eb
mahade <owork.hasan@gmail.com>
2026-04-11 11:01:23 +0600

    commited

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,11 @@
 const LeftSideNav = () => {
   return (
     <div className="border-2 border-gray-600">
-      <h2>Left Side Nav</h2>
+      <h2 className="mb-4 text-xl font-semibold">All Category</h2>
+
+      <p className="py-4 mb-2 text-lg font-semibold text-center rounded-lg bg-slate-200">
+        National News
+      </p>
     </div>
   );
 };
